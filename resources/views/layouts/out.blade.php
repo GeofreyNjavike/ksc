2b265e9a9b71a88f5e34448078cc6f45699e2c5a
geofey njavike <geofreynjavike@gmail.com>
2021-03-24 17:15:50 +0300

    first

---
+++ resources/views/layouts/out.blade.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Kisota Community sports Center</Center></title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+
+
+  <!-- Favicons -->
+  <link href="{{ asset('assets/img/favicon.png') }}" rel="icon">
+  <link href="{{ asset('assets/img/apple-touch-icon.png') }}"  rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{ asset('assets/vendor/animate.css/animate.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/aos/aos.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+
+</head>
+<body>
+    <header id="header" class="fixed-top">
+        <div class="container d-flex align-items-center justify-content-between">
+
+          <h1 class="logo"><a href="/">KSC</a></h1>
+          <!-- Uncomment below if you prefer to use an image logo -->
+          <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+          <nav id="navbar" class="navbar">
+            <ul>
+              <li><a class="nav-link scrollto active" href="#hero">Nyumbani</a></li>
+              <li><a class="nav-link scrollto" href="#about">Kuhusu</a></li>
+              <li><a class="nav-link scrollto" href="#services">Huduma</a></li>
+              <li><a class="nav-link scrollto " href="#portfolio">Wachezaji</a></li>
+              <li><a class="nav-link scrollto" href="#team">Team</a></li>
+              <li class="dropdown"><a href="#"><span>Profile</span> <i class="bi bi-chevron-down"></i></a>
+                <ul>
+
+                  <li class="dropdown"><a href="#"><span>Jiunge Nasi</span> <i class="bi bi-chevron-right"></i></a>
+                    <ul>
+                      <li><a href="#">Msajili Mwanao</a></li>
+                      <li><a href="#">Tutembelee</a></li>
+                      <li><a href="#">Tupigie</a></li>
+
+                    </ul>
+                  </li>
+                  @if (Route::has('login'))
+                  <div >
+                      @auth
+                         <li><a href="{{ url('/home') }}">Home</a></li>
+                      @else
+                          <li><a href="{{ route('login') }}">Login</a></li>
+                          @if (Route::has('register'))
+                              <li><a href="{{ route('register') }}">Register</a></li>
+                          @endif
+                      @endauth
+                  </div>
+              @endif
+                  <li><a href="#">Drop Down 3</a></li>
+                  <li><a href="#">Drop Down 4</a></li>
+                </ul>
+              </li>
+              <li><a class="nav-link scrollto" href="#contact">Mawasiliano</a></li>
+              <li><a class="getstarted scrollto" href="#about">Staff</a></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+          </nav><!-- .navbar -->
+
+        </div>
+      </header><!-- End Header -->
+    @yield('content')
+
+    <!-- Vendor JS Files -->
+  <script src="{{ asset('assets/vendor/aos/aos.js') }}"></script>
+  <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+  <script src="{{ asset('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+  <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
+  <script src="{{ asset('assets/vendor/purecounter/purecounter.js') }}"></script>
+  <script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{ asset('assets/js/main.js') }}"></script>
+</body>
+</html>
