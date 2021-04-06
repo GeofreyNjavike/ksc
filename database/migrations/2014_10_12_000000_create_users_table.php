66b0412869bceeb89731086107606c57d48c70ac
unknown <geofreynjavike@gmail.com>
2021-04-06 15:35:09 -0700

    done for today

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('fname');
+            $table->string('lname');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('clearance_level');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
